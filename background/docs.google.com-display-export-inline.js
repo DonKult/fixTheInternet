29cd7614a76ae5b2ef33dc20af1a5013ea997722
David Kalnischkies <david@kalnischkies.de>
2017-10-12 16:35:04 +0200

    docs.google.com: Display exports like PDF in the browser

---
+++ background/docs.google.com-display-export-inline.js
@@ -0,0 +1,19 @@
+"use strict";
+/* Don't download the exports, show them inline if possible – e.g. the PDF */
+(function() {
+function dispositionInlineForGoogleDocuments(e) {
+	e.responseHeaders.forEach(o => {
+		if (o.name.toLowerCase() !== 'Content-Disposition'.toLowerCase())
+			return;
+		const trigger = 'attachment;';
+		if (o.value.startsWith(trigger))
+			o.value = 'inline;' + o.value.substr(trigger.length);
+	});
+	return {responseHeaders: e.responseHeaders};
+}
+browser.webRequest.onHeadersReceived.addListener(
+	dispositionInlineForGoogleDocuments,
+	{urls: ['https://docs.google.com/document/export?id=*'] },
+	["blocking", "responseHeaders"]
+);
+})();

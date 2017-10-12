b2986c434c36e740ad0d059d1ce69ebb0984dd80
David Kalnischkies <david@kalnischkies.de>
2017-10-12 16:30:41 +0200

    bugs.debian.org: show attached patches inline

---
+++ background/bugs.debian.org-content-disposition.js
@@ -0,0 +1,35 @@
+"use strict";
+/* Firfox tries to make me download patches attached to bugreports,
+   but it would be nicer if I could have a quick look at them in the browser */
+(function() {
+function debianReplaceContentType(e) {
+	e.responseHeaders.forEach(o => {
+		if (o.name.toLowerCase() !== 'Content-Type'.toLowerCase())
+			return;
+		if (o.value.startsWith('text/x-diff'))
+			o.value = 'text/plain' + o.value.substr('text/x-diff'.length);
+		else if (o.value.startsWith('application/')) {
+			if (o.value === 'application/gzip') {
+				return;
+				/* Not allowed at the moment, sadly
+				// https://dxr.mozilla.org/mozilla-central/source/netwerk/protocol/http/HttpBaseChannel.cpp#1945
+				// make it look like a 'normal' page instead of an inlined download
+				e.responseHeaders.forEach(u => {
+					if (u.name.toLowerCase() !== 'Content-Disposition'.toLowerCase())
+						return;
+					u.name = 'Content-Encoding';
+					u.value = 'gzip';
+				});
+				//*/
+			}
+			o.value = 'text/plain; charset="utf-8"';
+		}
+	});
+	return {responseHeaders: e.responseHeaders};
+}
+browser.webRequest.onHeadersReceived.addListener(
+	debianReplaceContentType,
+	{urls: ['https://bugs.debian.org/cgi-bin/bugreport.cgi?att=*'] },
+	["blocking", "responseHeaders"]
+);
+})();
